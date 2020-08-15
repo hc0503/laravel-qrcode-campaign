b871ac421c967a5ff79f7c1a2dea9e9e81663fd9
Devsolution <q3construction1@gmail.com>
2020-08-15 14:11:52 +0800

    Update campaign user function

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -31,7 +31,7 @@ class Campaign extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::Class);
     }
 
 }
