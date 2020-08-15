208aeaabc022a3cdac952e1c2ba8be8f1bbba5a6
Devsolution <q3construction1@gmail.com>
2020-08-15 17:10:46 +0800

    Update coding style

--- resources/views/pages/campaigns/view.blade.php
+++ resources/views/pages/campaigns/view.blade.php
@@ -3,15 +3,15 @@
 @section('title', "View Campaign")
 
 @section('content')
-<!-- Start Campaign -->
-<div class="row">
-	<div class="col-lg-8 col-md-12 col-sm-12">
-		<div class="card">
-			<div class="card-header">
-			  <h4 class="card-title">@lang('locale.CampaignDetails')</h4>
-			</div>
-			<div class="card-content">
-			  <div class="card-body">
+	<!-- Start Campaign -->
+	<div class="row">
+		<div class="col-lg-8 col-md-12 col-sm-12">
+			<div class="card">
+				<div class="card-header">
+				<h4 class="card-title">@lang('locale.CampaignDetails')</h4>
+				</div>
+				<div class="card-content">
+				<div class="card-body">
 					<fieldset class="form-group">
 						<label for="campaign_name">@lang('locale.CampaignName')</label>
 						<input name="campaign_name" type="text" class="form-control" id="campaignName" readonly="readonly" value="{{ $campaign->campaign_name }}">
@@ -27,25 +27,25 @@
 							<a href="/campaigns" class="card-link">@lang('locale.PreviousCampaigns')</a>
 						</div>
 					</div>
-			  </div>
+				</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	
-	<div class="col-lg-4 col-md-12 col-sm-12">
-		<div class="card">
-			<div class="card-header">
-			  <h4 class="card-title">@lang('locale.QRCode')</h4>
-			</div>
-			<div class="card-content">
-			  	<div class="card-body">
-				  	<div class="row">
-						<img src="/qrcode/{{ $campaign->id }}" height="197px;">
-				  	</div>
+		
+		<div class="col-lg-4 col-md-12 col-sm-12">
+			<div class="card">
+				<div class="card-header">
+				<h4 class="card-title">@lang('locale.QRCode')</h4>
+				</div>
+				<div class="card-content">
+					<div class="card-body">
+						<div class="row">
+							<img src="/qrcode/{{ $campaign->id }}" height="197px;">
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<!--/ Start Campaign -->
+	<!--/ Start Campaign -->
 @endsection
