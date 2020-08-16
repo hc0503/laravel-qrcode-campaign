a0662ecd0e115732c1279582494e451e7b80ac85
Devsolution <q3construction1@gmail.com>
2020-08-16 19:46:54 +0800

    dashboard function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\CampaignHit;
 
 class DashboardController extends Controller
 {
+    // Default pageConfig
+    protected $pageConfigs = [
+        'navbarType' => 'sticky',
+        'footerType' => 'sticky'
+    ];
+
     // Dashboard - Analytics
     public function dashboardAnalytics(){
         $pageConfigs = [
@@ -14,6 +22,23 @@ class DashboardController extends Controller
 
         return view('/pages/dashboard-analytics', [
             'pageConfigs' => $pageConfigs
+        ]);
+    }
+
+    /**
+     * User dashboard
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function userDashboard()
+    {
+        $campaigns = Auth::user()->campaigns;
+        $campaignHits = CampaignHit::all();
+
+        return view('/pages/dashboard', [
+            'pageConfigs' => $this->pageConfigs,
+            'campaigns' => $campaigns,
+            'campaignHits' => $campaignHits
         ]);
     }
 
