49fdbbd38535e70c03019c82a17f3ee3548f61d4
Devsolution <q3construction1@gmail.com>
2020-08-17 16:55:16 +0800

    Url validation

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -58,10 +58,9 @@ class CampaignController extends Controller
      */
     public function store(Request $request)
     {
-        $regex = '_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:.\d{1,3}){3})(?!(?:169.254|192.168)(?:.\d{1,3}){2})(?!172.(?:1[6-9]|2\d|3[0-1])(?:.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]-)[a-z\x{00a1}-\x{ffff}0-9]+)(?:.(?:[a-z\x{00a1}-\x{ffff}0-9]-)[a-z\x{00a1}-\x{ffff}0-9]+)(?:.(?:[a-z\x{00a1}-\x{ffff}]{2,})).?)(?::\d{2,5})?(?:[/?#]\S)?$_iuS';
-        $validator = Validator::make($request->all(), [
+        $validator = $request->validate([
             'campaign_name' => 'required|string|max:190',
-            'url' => "required",
+            'url' => "required|url",
         ]);
 
         $campaign = $request->user()->campaigns()->create(
