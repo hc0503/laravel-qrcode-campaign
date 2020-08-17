d40f774ca52e7534a2e2ecbc66e2beee79a4bc9b
Devsolution <q3construction1@gmail.com>
2020-08-18 02:53:11 +0800

    create rolecontroller

---
+++ app/Http/Controllers/Admin/RoleController.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleController extends Controller
+{
+    // Default pageConfig
+    protected $pageConfigs = [
+        'navbarType' => 'sticky',
+        'footerType' => 'sticky',
+        'mainLayoutType' => 'horizontal',
+        'horizontalMenuType' => 'floating',
+        'theme' => 'dark',
+        'navbarColor' => 'bg-primary'
+    ];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    function __construct()
+    {
+        // $this->middleware(['auth', 'isAdmin']); //isAdmin middleware lets only users with a //specific permission permission to access these resources
+        // $this->middleware('permission:role-admin');
+        // $this->middleware('permission:role-create', ['only' => ['create','store']]);
+        // $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
+        // $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::all(); //Get all roles
+        $breadcrumbs = [
+            ['link'=>"/admin/roles",'name'=>trans('locale.role.title')], ['name'=>trans('locale.role.list')]
+        ];
+
+        return view('/pages/admin/roles/index', [
+            'pageConfigs' => $this->pageConfigs,
+            'breadcrumbs' => $breadcrumbs,
+            'roles' => $roles
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $permissions = Permission::all(); //Get all permissions
+        $breadcrumbs = [
+            ['link'=>"/admin/roles",'name'=>trans('locale.role.title')], ['name'=>trans('locale.role.create')]
+        ];
+        
+        return view('/pages/admin/roles/create', [
+            'pageConfigs' => $this->pageConfigs,
+            'breadcrumbs' => $breadcrumbs,
+            'permissions' => $permissions
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'=>'required|unique:roles|max:10',
+            'permissions' =>'required',
+            ]
+        );
+
+        $permissions = $request->permissions;
+
+        Role::create([
+            'name' => $request->name
+        ]);
+
+        foreach ($permissions as $permission) {
+            $p = Permission::where('id', '=', $permission)->firstOrFail(); 
+            //Fetch the newly created role and assign permission
+            $role = Role::where('name', '=', $request->name)->first(); 
+            $role->givePermissionTo($p);
+        }
+
+        return redirect()
+            ->route('roles.index')
+            ->with('message', trans('locale.role.message.save'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Spatie\Permission\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Role $role)
+    {
+        $permissions = Permission::get(); //Get all roles
+        $breadcrumbs = [
+            ['link'=>"/admin/roles",'name'=>trans('locale.role.title')], ['name'=>trans('locale.role.edit')]
+        ];
+        
+        return view('/pages/admin/roles/edit', [
+            'pageConfigs' => $this->pageConfigs,
+            'breadcrumbs' => $breadcrumbs,
+            'role' => $role,
+            'permissions' => $permissions
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Spatie\Permission\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        $this->validate($request, [
+            'name'=>'required|max:10|unique:roles,name,'.$role->id,
+            'permissions' =>'required',
+        ]);
+        
+        $permissions = $request->permissions;
+        
+        $role->update([
+            'name' => $request->name
+        ]);
+        
+        $p_all = Permission::all(); //Get all permissions
+
+        foreach ($p_all as $p) {
+            $role->revokePermissionTo($p); //Remove all permissions associated with role
+        }
+
+        foreach ($permissions as $permission) {
+            $p = Permission::where('id', '=', $permission)->firstOrFail(); //Get corresponding form //permission in db
+            $role->givePermissionTo($p);  //Assign permission to role
+        }
+
+        return redirect()->route('roles.index')
+            ->with('message', trans('locale.role.message.update'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Spatie\Permission\Models\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        $role->delete();
+
+        return redirect()
+            ->route('roles.index')
+            ->with('message', trans('locale.role.message.delete'));
+    }
+}
