b111cb7124f573143f12ce86cc0157925cdf155c
Devsolution <q3construction1@gmail.com>
2020-08-18 02:51:32 +0800

    permission seed

---
+++ database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+            'qrcode_manage',
+            'user_manage',
+        ];
+ 
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+}
