1c2c048c8c217531dc3c41bfaff4b2e3aa897e4e
Devsolution <q3construction1@gmail.com>
2020-08-18 04:00:28 +0800

    setattribute password

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,6 +38,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function setPasswordAttribute($password)
+    {   
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
