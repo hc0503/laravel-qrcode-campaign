a2d38ccdfade447183f2feb1d4e34ca6ea9ebe94
Devsolution <q3construction1@gmail.com>
2020-08-17 18:51:07 +0800

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,73 +27,49 @@
               <div class="card-body pt-1">
                 <form method="POST" action="{{ route('login') }}">
                   @csrf
-                  <fieldset class="form-label-group form-group position-relative has-icon-left">
-
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                      name="email" placeholder="E-Mail Address" value="{{ old('email') }}" required autocomplete="email"
-                      autofocus>
-
-                    <div class="form-control-position">
-                      <i class="feather icon-user"></i>
-                    </div>
-                    <label for="email">E-Mail Address</label>
+                  <fieldset class="form-label-group form-group position-relative">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="{{ trans('locale.enterEmail') }}" value="{{ old('email') }}" autofocus>
+                    <label for="email">@lang('locale.Email')</label>
                     @error('email')
                     <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
+                      {{ $message }}
                     </span>
                     @enderror
                   </fieldset>
 
-                  <fieldset class="form-label-group position-relative has-icon-left">
-
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                      name="password" placeholder="Password" required autocomplete="current-password">
-
-                    <div class="form-control-position">
-                      <i class="feather icon-lock"></i>
-                    </div>
-                    <label for="password">Password</label>
+                  <fieldset class="form-label-group position-relative">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="{{ trans('locale.enterPassword') }}" value="{{ old('password') }}">
+                    <label for="password">@lang('locale.Password')</label>
                     @error('password')
                     <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
+                      {{ $message }}
                     </span>
                     @enderror
                   </fieldset>
                   <div class="form-group d-flex justify-content-between align-items-center">
                     <div class="text-left">
-                      <fieldset class="checkbox">
-                        <div class="vs-checkbox-con vs-checkbox-primary">
-                          <input type="checkbox" {{ old('remember') ? 'checked' : '' }}>
-                          <span class="vs-checkbox">
-                            <span class="vs-checkbox--check">
-                              <i class="vs-icon feather icon-check"></i>
+                        <fieldset class="checkbox">
+                          <div class="vs-checkbox-con vs-checkbox-primary">
+                            <input type="checkbox" {{ old('remember') ? 'checked' : '' }}>
+                            <span class="vs-checkbox">
+                              <span class="vs-checkbox--check">
+                                <i class="vs-icon feather icon-check"></i>
+                              </span>
                             </span>
-                          </span>
-                          <span class="">Remember me</span>
-                        </div>
-                      </fieldset>
+                            <span class="">@lang('locale.RememberMe')</span>
+                          </div>
+                        </fieldset>
                     </div>
-                    @if (Route::has('password.request'))
-                    <div class="text-right"><a class="card-link" href="{{ route('password.request') }}">
-                        Forgot Password?
-                      </a></div>
-                    @endif
-
+                    <div class="text-right"><a href="{{ route('password.request') }}" class="card-link">Forgot Password?</a></div>
                   </div>
                   <a href="register" class="btn btn-outline-primary float-left btn-inline">Register</a>
                   <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
-                </form>
+              </form>
               </div>
             </div>
             <div class="login-footer">
               <div class="divider">
-                <div class="divider-text">OR</div>
-              </div>
-              <div class="footer-btn d-inline">
-                <a href="#" class="btn btn-facebook"><span class="fa fa-facebook"></span></a>
-                <a href="#" class="btn btn-twitter white"><span class="fa fa-twitter"></span></a>
-                <a href="#" class="btn btn-google"><span class="fa fa-google"></span></a>
-                <a href="#" class="btn btn-github"><span class="fa fa-github-alt"></span></a>
+                
               </div>
             </div>
           </div>
