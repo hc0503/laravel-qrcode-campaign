0c1a98a53aa1466d558b6886b2d9a2a2802bfa2c
Devsolution <q3construction1@gmail.com>
2020-08-18 03:36:08 +0800

    create usercontroller

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Auth;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class UserController extends Controller
+{
+    // Default pageConfig
+    protected $pageConfigs = [
+        'navbarType' => 'sticky',
+        'footerType' => 'sticky',
+        'mainLayoutType' => 'horizontal',
+        'horizontalMenuType' => 'floating',
+        'theme' => 'dark',
+        'navbarColor' => 'bg-primary'
+    ];
+
+    public function __construct() {
+        // $this->middleware(['auth', 'isAdmin']);//isAdmin middleware lets only users with a //specific permission permission to access these resources
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all(); //Get all users
+        $breadcrumbs = [
+            ['link'=>"/admin/users", 'name'=>trans('locale.user.title')], ['name'=>trans('locale.user.list')]
+        ];
+
+        return view('/pages/admin/users/index', [
+            'pageConfigs' => $this->pageConfigs,
+            'breadcrumbs' => $breadcrumbs,
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
