fc1b52c5f72376cb5935cc81ee0acb4e3a781514
Devsolution <q3construction1@gmail.com>
2020-08-18 09:31:42 +0800

    role assign

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -83,10 +83,7 @@ class UserController extends Controller
         $roles = $request->roles; //Retrieving the roles field
         //Checking if a role was selected
         if (isset($roles)) {
-            foreach ($roles as $role) {
-                $role_r = Role::where('id', '=', $role)->firstOrFail();            
-                $user->assignRole($role_r); //Assigning role to user
-            }
+            $user->assignRole($roles); //Assigning role to user
         }        
         //Redirect to the users.index view and display message
         return redirect()->route('users.index')
@@ -107,34 +104,65 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \Spatie\Permission\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        $roles = Role::all(); //Get all roles
+
+        $breadcrumbs = [
+            ['link'=>"/admin/users",'name'=>trans('locale.user.title')], ['name'=>trans('locale.user.edit')]
+        ];
+        
+        return view('/pages/admin/users/edit', [
+            'pageConfigs' => $this->pageConfigs,
+            'breadcrumbs' => $breadcrumbs,
+            'roles' => $roles,
+            'user' => $user
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Spatie\Permission\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $this->validate($request, [
+            'name'=>'required|max:120',
+            'surname'=>'required|max:120',
+            'email'=>'required|email|unique:users,email,'.$user->id,
+        ]);
+
+        $user->update($request->all());
+        $roles = $request->roles;
+
+        if (isset($roles)) {
+            $user->syncRoles($roles);
+        } else {
+            $user->roles()->detach(); //If no role is selected remove exisiting role associated to a user
+        }
+        
+        return redirect()->route('users.index')
+            ->with('message', trans('locale.user.message.update'));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \Spatie\Permission\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect()
+            ->route('users.index')
+            ->with('message', trans('locale.user.message.delete'));
     }
 }
