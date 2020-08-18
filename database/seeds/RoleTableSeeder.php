a9a8280657640225cbf45f66599bace7aa55d36c
Devsolution <q3construction1@gmail.com>
2020-08-18 09:29:48 +0800

    role seeder

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create(['name' => 'Admin']);
+        $role->givePermissionTo('user_manage');
+
+        $role = Role::create(['name' => 'User']);
+        $role->givePermissionTo('qrcode_manage');
+    }
+}
