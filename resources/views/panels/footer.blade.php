496303b46938b69bb1c21cbb6ee0a6e5b4879715
kcaribou <kcaribou0428@gmail.com>
2020-08-19 11:17:48 -0700

    footerbar change

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -7,9 +7,9 @@
         class="footer {{ $configData['footerType'] }}  {{($configData['footerType']=== 'footer-hidden') ? 'd-none':''}} footer-light">
         @endif
         <p class="clearfix blue-grey lighten-2 mb-0"><span
-                class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2019<a
-                    class="text-bold-800 grey darken-2" href="https://1.envato.market/pixinvent_portfolio"
-                    target="_blank">Pixinvent,</a>All rights Reserved</span><span
+                class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2020<a
+                    class="text-bold-800 grey darken-2" href="/"
+                    target="_blank">QR Code,</a>All rights Reserved</span><span
                 class="float-md-right d-none d-md-block">Hand-crafted & Made with<i
                     class="feather icon-heart pink"></i></span>
             <button class="btn btn-primary btn-icon scroll-top" type="button"><i
