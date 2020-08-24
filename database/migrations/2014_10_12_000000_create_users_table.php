17e9536809fd70a3aa8c3cb4cc454429a5af1ae7
Devsolution <q3construction1@gmail.com>
2020-08-24 17:06:42 +0800

    update existed migration in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
