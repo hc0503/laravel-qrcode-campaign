d1ff7d02b7fc63eab22a20c934b07a4848b41cc1
Devsolution <q3construction1@gmail.com>
2020-08-24 14:23:11 +0800

    default user role

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,12 +64,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'surname' => $data['surname'],
             'email' => $data['email'],
             'password' => $data['password'],
         ]);
+
+        $user->assignRole("User");
+        
+        return $user;
     }
 
     // Register
