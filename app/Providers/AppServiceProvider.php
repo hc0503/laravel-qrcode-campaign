32c8e8b65b7e9e0e1f83aa0b22c1ff2a620e6a4d
Salvatore Graci <sgb.coding@gmail.com>
2023-04-29 00:47:49 +0200

    Changed QRGenerator to python script

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        URL::forceScheme('https');
+        if(env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
--- artisan.sh
+++ artisan.sh
@@ -4,9 +4,9 @@ cd /app
 wait-for-it db:3306 -t 45
 
 if [ "$FIRST_RUN" == "true" ]; then
-    echo yes | php artisan migrate
-    php artisan db:seed
     php artisan key:generate
+    echo yes | php artisan migrate
+    echo yes | php artisan db:seed
     sed '/FIRST_RUN=true/c\FIRST_RUN=false' ./.env.sys > .new_env
     cat .new_env > .env.sys
     rm -f .new_env
