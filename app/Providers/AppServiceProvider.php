147dffef0183179f24faa05818cfb2e9468a1279
Luca Steccanella <steccas@pm.me>
2023-04-28 15:52:52 +0200

    temp remove forcing https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        URL::forceScheme('https');
+        // URL::forceScheme('https');
     }
 }
