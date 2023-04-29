49cfb2740608c14d4a5d6d89235e6c376bab58ff
Luca Steccanella <luca.steccanella@semiodigital.it>
2023-04-29 18:38:58 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,12 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-<<<<<<< HEAD
         if(env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
-=======
-        // URL::forceScheme('https');
->>>>>>> origin/php8-py
     }
 }
